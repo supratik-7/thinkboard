c98186cc4dd810bb599b21671b9f79b603e7a17d
supratik-7 <supratikdatta2@gmail.com>
2025-06-26 22:21:56 +0530

    prepared for the development

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,16 +13,18 @@ dotenv.config();
 
 const app= express();
 const PORT= process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 
 
 // middleware
-app.use(
+if (process.env.NODE_ENV !== "production") {
+  app.use(
     cors({
-    origin: "http://localhost:5173",
-    
-})
-);
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 
 app.use(rateLimiter);
@@ -35,6 +37,14 @@ app.use(rateLimiter);
 
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
 app.listen(PORT, () => {
